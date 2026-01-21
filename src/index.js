c4eaf22dd27db3d4b32cde86fa15732bb33d7d27
komal.kumari@antino.com <komal>
2026-01-21 13:27:26 +0530

    defined mongoURI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Task = require("./models/Task");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
